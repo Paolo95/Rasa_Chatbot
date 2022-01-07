a95e5f0d6fe82319be8c2dbfeab8a90e4ffbc511
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-01-07 17:33:12 +0100

    Implementato l'intestatario della carta nel form

--- actions/actions.py
+++ actions/actions.py
@@ -14,6 +14,7 @@ pattern_diners = "^3(?:0[0-5]|[68][0-9])[0-9]{11}$"
 pattern_mastercard = "^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$"
 pattern_jcb = "^(?:2131|1800|35\d{3})\d{11}$"
 pattern_union = "^(62[0-9]{14,17})$"
+pattern_intestatario = "^([a-z]+[,.]?[ ]?|[a-z]+['-]?)+$"
 
 
 class ValidazioneFormOrdine(FormValidationAction):
@@ -63,25 +64,39 @@ class ValidazioneFormOrdine(FormValidationAction):
         domain: DomainDict,
     ) -> Dict[Text, Any]:
         """Validate 'numero_carta'."""
-
-        if re.match(pattern_visa,str(slot_value)) or re.match(pattern_visa_mastercard,str(slot_value)) is not None:
+        string_card = str(slot_value).replace(" ","").replace("-","")
+        if re.match(pattern_visa,str(string_card)) or re.match(pattern_visa_mastercard,str(string_card)) is not None:
             dispatcher.utter_message(text=f"Il numero della carta VISA o VISA MASTERCARD è corretto!")
             return {"numero_carta": slot_value}          
-        elif re.match(pattern_diners, str(slot_value)) is not None:
+        elif re.match(pattern_diners, str(string_card)) is not None:
             dispatcher.utter_message(text=f"Il numero della carta DINERS è corretto!")
             return {"numero_carta": slot_value}
-        elif re.match(pattern_mastercard, str(slot_value)) is not None:
+        elif re.match(pattern_mastercard, str(string_card)) is not None:
             dispatcher.utter_message(text=f"Il numero della carta MASTERCARD è corretto!")
             return {"numero_carta": slot_value}
-        elif re.match(pattern_jcb, str(slot_value)) is not None:
+        elif re.match(pattern_jcb, str(string_card)) is not None:
             dispatcher.utter_message(text=f"Il numero della carta JCB è corretto!")
             return {"numero_carta": slot_value}
-        elif re.match(pattern_union, str(slot_value)) is not None:
+        elif re.match(pattern_union, str(string_card)) is not None:
             dispatcher.utter_message(text=f"Il numero della carta UNION è corretto!")
             return {"numero_carta": slot_value}
         else:           
             dispatcher.utter_message(text=f"Il numero di carta è errato, riprova")
             return {"numero_carta":None}            
-                    
-                
+        
+    def validate_intestatario_carta(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'intestatario_carta'."""
+
+        if re.match(pattern_intestatario,str(slot_value)) is not None:
+            dispatcher.utter_message(text=f"Inserisci un intestatario valido!")
+            return {"intestatario_carta":None}
+        dispatcher.utter_message(text=f"OK!")
+        return {"intestatario_carta": slot_value}
+
         return []
