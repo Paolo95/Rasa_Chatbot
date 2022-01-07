a3401d821a492a50488cc2ab2965a27673dd4cf5
Paolo Compagnoni <compagnonipaolo95@gmail.com>
2022-01-07 02:55:45 +0100

    Introdotto il form (bug da risolvere)
    Il form risponde e memorizza le domande base.
    BUG: non carica l'action sulla carta di credito

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,68 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+import re
+
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import EventType
+from rasa_sdk.types import DomainDict
+
+VALORI_CONSENTITI_CIRCUITO = ["visa", "mastercard", "diners", "american express", "china union pay"]
+VALORI_CONSENTITI_PC = ["notebook", "pc"]
+pattern = '([0-9]+( [0-9]+)+)'
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ValidazioneFormOrdine(FormValidationAction):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "validate_form_ordine"
+
+    def validate_circuito(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'circuito'."""
+
+        if slot_value.lower() not in VALORI_CONSENTITI_CIRCUITO:
+            dispatcher.utter_message(text=f"Si accettano solo i seguenti circuiti: VISA, MASTERCARD, DINERS, AMERICAN EXPRESS, JCB e CHINA UNION PAY")
+            return {"circuito":None}
+        dispatcher.utter_message(text=f"OK! Il circuito della tua carta di credito è stato accettato!")
+        return {"circuito": slot_value}
+
+        return []
+    
+    def validate_tipologia_prodotto(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'tipologia_prodotto'."""
+
+        if slot_value.lower() not in VALORI_CONSENTITI_PC:
+            dispatcher.utter_message(text=f"Si accettano solo ordini relativi a PC e notebook!")
+            return {"tipologia_prodotto":None}
+        dispatcher.utter_message(text=f"OK!")
+        return {"tipologia_prodotto": slot_value}
+
+        return []
+
+    def validate_numero_carta(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate 'numero_carta'."""
+        
+        if re.match(pattern, slot_value):
+            dispatcher.utter_message(text=f"Numero di carta errato, riprova")
+            return {"numero_carta":None}
+        dispatcher.utter_message(text=f"Il numero della carta è corretto!")
+        return {"numero_carta": slot_value}
+
+        return []
